f68bb8ac48436c534ac868fee608bec6127a23ec
lamttKan <lamtthe150351@fpt.edu.vn>
2024-02-20 21:20:45 +0700

    20/2 CRUD Service

--- DentalClinicProject/Program.cs
+++ DentalClinicProject/Program.cs
@@ -1,3 +1,6 @@
+using DentalClinicProject.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<dentalContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +25,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors(x => x
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials()
+           .SetIsOriginAllowed(origin => true));
 app.Run();
