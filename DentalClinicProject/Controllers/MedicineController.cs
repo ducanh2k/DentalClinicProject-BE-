b5effde73d936ec9156d80095c5d8b37c135e229
lamttKan <lamtthe150351@fpt.edu.vn>
2024-02-21 20:48:44 +0700

    21/2 add MedicineController

---
+++ DentalClinicProject/Controllers/MedicineController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinicProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
