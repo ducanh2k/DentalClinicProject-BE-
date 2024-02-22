bec21a0834ca31682958efd8f8e9c7c9d0d2e5da
dungtxhe141059 <dungtxhe141059@fpt.edu.vn>
2024-02-22 22:52:45 +0700

    appoitment v1

---
+++ DentalClinicProject/DTO/AppointmentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DentalClinicProject.DTO
+{
+    public class AppointmentDTO
+    {
+        public int? EmployeeId { get; set; }
+        public int? PatientId { get; set; }
+        public DateTime? Datetime { get; set; }
+        public string? Note { get; set; }
+        public bool? Status { get; set; }
+    }
+}
