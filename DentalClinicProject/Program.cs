114b73a1346c40ec5a8117349e83cf805ebb4821
đức anh trần <tdanhaltt18@gmail.com>
2024-02-27 15:54:19 +0700

    .

--- DentalClinicProject/Program.cs
+++ DentalClinicProject/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MyMapperProfile));
 builder.Services.AddDbContext<dentalContext>(
     option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
 
