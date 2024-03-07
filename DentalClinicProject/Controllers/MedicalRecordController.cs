abdc79f86f1233e71e88d6f0f65eda2936daa0a5
lamttKan <lamtthe150351@fpt.edu.vn>
2024-03-07 10:36:52 +0700

    7/3 lam v1

--- DentalClinicProject/Controllers/MedicalRecordController.cs
+++ DentalClinicProject/Controllers/MedicalRecordController.cs
@@ -60,6 +60,9 @@ namespace DentalClinicProject.Controllers
                     .ThenInclude(md => md.Prescription)
                 .Include(mr => mr.MedicalRecordDetails)
                     .ThenInclude(md => md.Service)
+                .Include(mr => mr.MedicalRecordDetails)
+                    .ThenInclude(md => md.Appointments) // Include appointments
+                    .ThenInclude(appointment => appointment.Doctor) // Include doctor of the appointment
                 .Where(mr => mr.MedicalRecordId == id && mr.DeleteFlag == false)
                 .Select(m => new
                 {
@@ -76,7 +79,11 @@ namespace DentalClinicProject.Controllers
                         o.ServiceId,
                         ServiceName = o.Service.ServiceName,
                         o.PrescriptionId,
-                        o.Diagnosis
+                        o.Diagnosis,
+                        Appointments = o.Appointments.Select(appointment => new
+                        {
+                            appointment.AppointmentId
+                        })
                     }
                     )
 
@@ -123,7 +130,20 @@ namespace DentalClinicProject.Controllers
                         o.ServiceId,
                         ServiceName = o.Service.ServiceName,
                         o.PrescriptionId,
-                        o.Diagnosis
+                        o.Diagnosis,
+                        Appointments = o.Appointments.Select(appointment => new
+                        {
+                            appointment.AppointmentId,
+                            appointment.EmployeeId,
+                            EmployeeName = appointment.Employee.Name,
+                            appointment.PatientId,
+                            appointment.Datetime,
+                            appointment.Note,
+                            appointment.Status,
+                            appointment.DeleteFlag,
+                            appointment.DoctorId,
+                            DoctorName = appointment.Doctor.Name,
+                        })
                     }
                     )
                     
