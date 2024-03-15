7cae043660b59d5c1771eb5b3239e943b03d5957
lamtt1 <lamtthe150351@fpt.edu.vn>
2024-03-15 16:06:14 +0700

    15/3 dashboard v2

--- DentalClinicProject/Controllers/DashboardController.cs
+++ DentalClinicProject/Controllers/DashboardController.cs
@@ -34,14 +34,12 @@ namespace DentalClinicProject.Controllers
                              from inv in invJoin.DefaultIfEmpty()
                              join s in _context.Services on il.ServiceId equals s.ServiceId into sJoin
                              from s in sJoin.DefaultIfEmpty()
-                             join m in _context.Materials on il.MaterialId equals m.MaterialId into mJoin
-                             from m in mJoin.DefaultIfEmpty()
                              where inv != null && inv.Status == 2 && inv.Date.Value.Year == year
                              select new
                              {
                                  Year = inv.Date.Value.Year,
                                  Month = inv.Date.Value.Month,
-                                 TotalPay = (s.Price ?? 0) + (m.UnitPrice * il.Quantity ?? 0)
+                                 TotalPay = (s.Price ?? 0) 
                              }).ToList();
 
                 var result = query.GroupBy(x => new { x.Year, x.Month })
@@ -71,15 +69,13 @@ namespace DentalClinicProject.Controllers
                             from inv in invGroup.DefaultIfEmpty()
                             join service in _context.Services on invLine.ServiceId equals service.ServiceId into serviceGroup
                             from service in serviceGroup.DefaultIfEmpty()
-                            join material in _context.Materials on invLine.MaterialId equals material.MaterialId into materialGroup
-                            from material in materialGroup.DefaultIfEmpty()
                             where inv.Date.Value.Year == CurrentYear && inv.Date.Value.Month == CurrentMonth && inv.Status == 2
                             select new
                             {
                                 Year = inv.Date.Value.Year,
                                 Month = inv.Date.Value.Month,
                                 Day = inv.Date.Value.Day,
-                                TotalPay = (service.Price ?? 0) + (material.UnitPrice * invLine.Quantity ?? 0)
+                                TotalPay = (service.Price ?? 0) 
                             }
                         into subquery
                             group subquery by new { subquery.Year, subquery.Month, subquery.Day } into g
@@ -186,7 +182,55 @@ namespace DentalClinicProject.Controllers
         }
 
         //Chi phi moi trang thiet bi trong 1 thang (12 thang)
-
+        [HttpGet("expense")]
+        public IActionResult GetExpense(int month, int year) // month = 0 mean service in all year
+        {
+            try
+            {
+                var query = (from il in _context.InvoiceLines
+                             join inv in _context.Invoices on il.InvoiceId equals inv.InvoiceId into invJoin
+                             from inv in invJoin.DefaultIfEmpty()
+                             join m in _context.Materials on il.MaterialId equals m.MaterialId into mJoin
+                             from m in mJoin.DefaultIfEmpty()
+                             where inv != null && inv.Status == 2 && inv.Date.Value.Year == year
+                             select new
+                             {
+                                 Year = inv.Date.Value.Year,
+                                 Month = inv.Date.Value.Month,
+                                 TotalSpent = (m.UnitPrice * il.Quantity ?? 0)
+                             }).ToList();
+                if(month == 0)
+                {
+                    var result = query.Where(x => x.Year == year)
+                                                      .GroupBy(x => new { x.Year, x.Month })
+                                                      .Select(g => new
+                                                      {
+                                                          Year = g.Key.Year,
+                                                          Month = g.Key.Month,
+                                                          TotalSpentOfMonth = g.Sum(x => x.TotalSpent)
+                                                      })
+                                                      .ToList<object>();
+                    return Ok(result);
+                }else
+                {
+                    var result = query.Where(x => x.Month == month && x.Year == year)
+                                  .GroupBy(x => new { x.Year, x.Month })
+                                  .Select(g => new
+                                  {
+                                      Year = g.Key.Year,
+                                      Month = g.Key.Month,
+                                      TotalPayOfMonth = g.Sum(x => x.TotalSpent)
+                                  })
+                                  .ToList<object>();
+                    return Ok(result);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
         //top 8 service duoc dung nhieu nhat all time
         [HttpGet("topService")]
@@ -195,20 +239,18 @@ namespace DentalClinicProject.Controllers
             try
             {
                 var result = (from il in _context.InvoiceLines
-                              join m in _context.Materials on il.MaterialId equals m.MaterialId
-                              group new { il, m } by new { il.MaterialId, m.MaterialName } into g
+                              join i in _context.Invoices on il.InvoiceId equals i.InvoiceId
+                              group il by il.ServiceId into g
+                              orderby g.Count() descending
                               select new
                               {
-                                  MaterialId = g.Key.MaterialId,
-                                  MaterialName = g.Key.MaterialName,
-                                  TotalQuantity = g.Sum(x => x.il.Quantity),
-                                  TotalPay = g.Sum(x => x.m.UnitPrice * x.il.Quantity)
-                              })
-                         .OrderByDescending(x => x.TotalQuantity)
-                         .Take(8)
-                         .ToList();
+                                  ServiceId = g.Key,
+                                  UsageCount = g.Count()
+                              }).Take(8);
+
                 return Ok(result);
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
