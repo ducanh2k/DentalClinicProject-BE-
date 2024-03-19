8f167c95dcaa964de803ad705064f61cf9117fe1
lamtt1 <lamtthe150351@fpt.edu.vn>
2024-03-19 20:36:17 +0700

    19/3 restruct Medicine

--- DentalClinicProject/Services/Interface/IMedicineService.cs
+++ DentalClinicProject/Services/Interface/IMedicineService.cs
@@ -1,6 +1,15 @@
-﻿namespace DentalClinicProject.Services.Interface
+﻿using DentalClinicProject.DTO;
+using DentalClinicProject.Models;
+
+namespace DentalClinicProject.Services.Interface
 {
     public interface IMedicineService
     {
+        List<Medicine> GetMedicines(int pageNumber);
+        List<Medicine> GetMedicinesByName(string keyword, int pageNumber);
+        Medicine GetMedicine(int id);
+        void AddMedicine(MedicineDTO med);
+        void UpdateMedicine(int id, MedicineDTO medDTO);
+        void DeleteMedicine(int id);
     }
 }
